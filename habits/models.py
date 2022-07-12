6c7c0ee71c47348b4c7df5a9f802f480211752a3
andres alcocer <devandres.tech@gmail.com>
2022-07-12 07:54:38 -0400

    set up models

--- habits/models.py
+++ habits/models.py
@@ -3,8 +3,29 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 class User(AbstractUser):
     pass
 
     def __str__(self):
         return self.username
+
+
+class Habit(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    target = models.IntegerField()
+
+
+class Record(BaseModel):
+    date = models.DateField(blank=True, null=True)
+    daily_number = models.IntegerField()
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+
